b1723b713a41b283f9647db6ad7a8b3754c58558
SHMOOOK <aml_1423@hotmail.com>
2026-08-25 20:58:27 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,11 +1,50 @@
+import os
+import requests
+
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
+API_KEY = os.getenv("STREAMPAY_API_KEY")
+
+
+@app.route("/")
+def home():
+    return "Value Club Webhook Running"
+
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
+
+    data = request.json
+
+    print("Webhook Received:")
+    print(data)
+
+    try:
+
+        payment_url = data.get("entity_url")
+
+        if payment_url:
+
+            response = requests.get(
+                payment_url,
+                headers={
+                    "Authorization": f"Bearer {API_KEY}"
+                }
+            )
+
+            print("Payment Details:")
+            print(response.json())
+
+    except Exception as e:
+
+        print("ERROR:")
+        print(str(e))
+
     return "OK", 200
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
