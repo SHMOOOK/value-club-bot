cdfb1186577e929356c719d51a7056ce49d0c37c
SHMOOOK <aml_1423@hotmail.com>
2026-08-26 13:16:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,13 +56,13 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
             ],
             [
                 InlineKeyboardButton(
-                    "⭐ نصف سنوي بخصم 10% - 264.60 ريال بدلا من 294 ريال",
+                    "⭐ نصف سنوي بخصم 10%(264.60 ريال بدلا من 294 ريال)",
                     callback_data="semi"
                 )
             ],
             [
                 InlineKeyboardButton(
-                    "🏆 سنوي بخصم 20% - 470.70 ريال بدلا من 588 ريال",
+                    "🏆 سنوي بخصم 20%(470.70 ريال بدلا من 588 ريال)",
                     callback_data="year"
                 )
             ]
