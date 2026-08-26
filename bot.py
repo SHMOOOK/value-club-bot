2fbf5b73725055cac2555217eb9db93dca2d7516
SHMOOOK <aml_1423@hotmail.com>
2026-08-26 12:56:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,13 +56,13 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
             ],
             [
                 InlineKeyboardButton(
-                    "⭐ اشتراك نصف سنوي - 264.60 ريال",
+                    "⭐ اشتراك نصف سنوي - بخصم 10% - 264.60 ريال",
                     callback_data="semi"
                 )
             ],
             [
                 InlineKeyboardButton(
-                    "🏆 اشتراك سنوي - 470.70 ريال",
+                    "🏆 اشتراك سنوي - بخصم 20% - 470.70 ريال",
                     callback_data="year"
                 )
             ]
